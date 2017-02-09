c1384df18dce79ea2e27304babe87e9be19bdb19
Chris <nixshell@yahoo.com>
2017-02-09 00:36:18 -0700

    key adn stuff

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
-const sass = require('node-sass-middleware');
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
@@ -49,10 +48,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(expressStatusMonitor());
 app.use(compression());
-app.use(sass({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public')
-}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
